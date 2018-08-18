a28d034a7bd45f71ed99a9c6ea66b99a77afdb08
Japnit Kaur Ahuja <japnit.ahuja@gmail.com>
2018-08-18 22:35:04 +0800

    Graph classification

--- Classifying LRs/Kmeans.py
+++ Classifying LRs/Kmeans.py
@@ -1,19 +1,33 @@
-import json
-from pprint import pprint
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import numpy as np
+#raw data has the dataset name too
+raw_data = open("values.txt").read().split("\n")
+raw_data = [list(map(str, i.split(","))) for i in raw_data]
+dataset_names = [i[3] for i in raw_data]
+for i in range(len(raw_data)):
+    raw_data[i].pop()
+raw_data = np.array(raw_data).astype('float') 
+kmeans = KMeans(n_clusters = 8, random_state=0).fit(raw_data) #what's random seed
 
-with open('_values.json') as data_file:    
-    data = json.load(data_file)
+labels = kmeans.labels_
 
-data_file.close()
-temp = []
+clusters = 8
+data = [[] for i in range(0,clusters)]
 
-opfile = open("values.txt","w")
+for i in range(len(raw_data)):
+    data[labels[i]].append(raw_data[i])
+
+for i in range(len(raw_data)):
+    print(dataset_names[i] + " " * (20 - len(dataset_names[i])) + str(labels[i]))
+print(labels)
+
+"""
+plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+plt.axis([0, 6, 0, 20])
+plt.show()
+"""
+
+
     
-for i in range(len(data)):
-    name = data[i]
-    a = data[i]["result"]["Random Forest"]["coefficients"][0]
-    b = data [i]["result"]["Random Forest"]["coefficients"][1]
-    alpha = data [i]["result"]["Random Forest"]["coefficients"][2]
-    temp = [name, a, b, alpha]
-
-opfile.close()
+    
