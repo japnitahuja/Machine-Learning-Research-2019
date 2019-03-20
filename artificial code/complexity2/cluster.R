445149f7da8d296b22f111f5827eb6da6f8a996d
Liu Ya <liuya2360@gmail.com>
2019-03-20 08:39:04 +0700

    artificial codes

---
+++ artificial code/complexity2/cluster.R
@@ -0,0 +1,33 @@
+library(ECoL)
+
+
+home = getwd()
+
+dataset_folder = file.path(home ,"cluster_dataset")
+count <- 0
+for (dataset in list.files(dataset_folder))
+{
+  count <- count + 1
+  print(dataset)
+  mydata <- read.csv(file.path(dataset_folder,dataset), header = FALSE)
+  file_op <- file.path(home , "/complexity2_cluster.txt")
+  m = length(mydata)
+  error = try(complexity(mydata[,1:(m-1)],mydata[,m],type="class"))
+  print(class(error))
+  if (class(error) != "try-error")
+  {
+    print("DONE")
+    write.table(dataset, file_op,sep = ",",append=T)
+    write.table(complexity(mydata[,1:(m-1)],mydata[,m],type="class"), file_op,sep = ",",append=T)
+  }
+  print(count)
+  
+}
+
+
+  
+
+
+
+
+
